e8aee3f127b7111d8e2b19224fa9f2d98192aa4f
Anthony <49073356+TXMack713@users.noreply.github.com>
2019-08-26 14:39:46 -0500

    Add files via upload
    Module 1 (Coding from scratch) - Lesson 7 JavaScript Part 2

---
+++ array_activity_script.js
@@ -0,0 +1,10 @@
+amazingCars=["BMW", "Lexus", "Ford", "Mercedes"];
+console.log("Here are some amazing cars");
+console.log(amazingCars);
+console.log("Here's that list again, but this time with the last car removed");
+amazingCars.pop();
+console.log(amazingCars);
+console.log("Here's the sorted version of the updated list:");
+console.log(amazingCars.sort());
+console.log("Here is the number of cars in the final list");
+console.log(amazingCars.length);
---
+++ lesson7_handson_index.js
@@ -0,0 +1,11 @@
+var awesomeBirds=["Bluebird", "Roadrunner", "Desert Dove", "Pigeon"];
+for(var i=0; i<awesomeBirds.length; i++){
+  if(awesomeBirds[i]==="Bluebird"){
+    alert("It's a " + awesomeBirds[i]);
+    alert("Bluebirds are so beautiful");
+  }
+  else{
+    alert("It's a " + awesomeBirds[i]);
+    alert("Not as beautiful as Bluebirds");
+  }
+}
